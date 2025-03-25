28f800b878eee25a0d340c1da93bf90efed669d7
dmanoilenko <manoylenko2001@icloud.com>
2025-03-25 23:38:01 +0200

    task3

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+const input = document.querySelector("#name-input");
+let userName = document.getElementById("name-output");
+input.addEventListener("input", (event) => {
+    let userValue = event.target.value.trim();
+    if (userValue === "") {
+        return userName.textContent = "Anonymous";
+     }
+     return userName.innerHTML = userValue;    
+})
+

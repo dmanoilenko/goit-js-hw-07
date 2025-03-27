51a99530a6d1cd871aefa04e03df10fb37428af6
dmanoilenko <manoylenko2001@icloud.com>
2025-03-27 22:09:59 +0200

    task5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,11 @@
+const bodyEl = document.querySelector("body");
+const colorArea = document.querySelector(".color");
+const button = document.querySelector(".change-color");
+button.addEventListener("click", getRandomHexColor)
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+  const randomColor = `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+    bodyEl.style.backgroundColor = randomColor;
+    colorArea.innerHTML = randomColor;
 }

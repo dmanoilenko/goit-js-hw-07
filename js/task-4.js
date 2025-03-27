5b36df529796588d632c4d44bcb3f6cbdd728074
dmanoilenko <manoylenko2001@icloud.com>
2025-03-27 20:59:46 +0200

    task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+    event.preventDefault();
+    const elements = event.target.elements;
+    const userData = {
+        email: elements.email.value.trim(),
+        password: elements.password.value.trim()
+    }
+    console.log(userData);
+    if (elements.email.value === "" || elements.password.value === "") {
+        alert('All form fields must be filled in')
+    }
+    event.target.reset()
+}
 
